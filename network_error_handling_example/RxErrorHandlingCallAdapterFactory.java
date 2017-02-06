faf554c53c8e7345601e2cbf313a4690a4a73e7d
Boris Sidorov <sergey@lodossteam.com>
2017-02-06 11:40:04 +0300

    Added example of handling errors in Retrofit 2

---
+++ network_error_handling_example/RxErrorHandlingCallAdapterFactory.java
@@ -0,0 +1,81 @@
+import com.dayteam.goeng.exception.RetrofitException;
+import com.jakewharton.retrofit2.adapter.rxjava2.HttpException;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import io.reactivex.Single;
+import io.reactivex.SingleSource;
+import io.reactivex.functions.Function;
+import retrofit2.Call;
+import retrofit2.CallAdapter;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+/**
+ * Factory, which allows to handle network related errors
+ * Should be added to Retrofit instance as defaul CallAdapterFactory
+ */
+
+public class RxErrorHandlingCallAdapterFactory extends CallAdapter.Factory {
+    private final RxJava2CallAdapterFactory original;
+
+    public RxErrorHandlingCallAdapterFactory() {
+        original = RxJava2CallAdapterFactory.create();
+    }
+
+    public static CallAdapter.Factory create() {
+        return new RxErrorHandlingCallAdapterFactory();
+    }
+
+    @Override
+    public CallAdapter<?> get(Type returnType, Annotation[] annotations, Retrofit retrofit) {
+        return new RxCallAdapterWrapper(retrofit, original.get(returnType, annotations, retrofit));
+    }
+
+    private static class RxCallAdapterWrapper implements CallAdapter<Single<?>> {
+        private final Retrofit retrofit;
+        private final CallAdapter<?> wrapped;
+
+        public RxCallAdapterWrapper(Retrofit retrofit, CallAdapter<?> wrapped) {
+            this.retrofit = retrofit;
+            this.wrapped = wrapped;
+        }
+
+        @Override
+        public Type responseType() {
+            return wrapped.responseType();
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <R> Single<?> adapt(Call<R> call) {
+            return ((Single) wrapped.adapt(call)).onErrorResumeNext(new Function<Throwable, SingleSource>() {
+                @Override
+                public SingleSource apply(Throwable throwable) throws Exception {
+                    return Single.error(asRetrofitException(throwable));
+                }
+            });
+        }
+
+        private RetrofitException asRetrofitException(Throwable throwable) {
+            // We had non-200 http error
+            if (throwable instanceof HttpException) {
+                HttpException httpException = (HttpException) throwable;
+                Response response = httpException.response();
+                return RetrofitException.httpError(response.raw().request().url().toString(), response, retrofit);
+            }
+            // A network error happened
+            if (throwable instanceof IOException) {
+                return RetrofitException.networkError((IOException) throwable);
+            }
+
+            // We don't know what happened. We need to simply convert to an unknown error
+
+            return RetrofitException.unexpectedError(throwable);
+        }
+    }
+
+}
